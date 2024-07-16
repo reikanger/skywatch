f00964d6086812b433cf4b3a513aa72ed32c0b9e
Melissa Judy <163203732+MelissaJ4@users.noreply.github.com>
2024-07-15 21:09:35 -0500

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "65ae2dac6335928b26fc46a0be1d3f93"
+weather_api_key = ""
 
 # AviationEdge API Key
-aviation_edge_api_key = "b463bd-1a4e0d"
+aviation_edge_api_key = ""
